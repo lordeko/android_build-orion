fddf1e699c039d7f9f7206724d522877c6fcc0a5
Soham Jambhekar <soham4jam@gmail.com>
2015-06-12 20:19:20 -0400

    add orion dependencies

--- tools/roomservice.py
+++ tools/roomservice.py
@@ -43,7 +43,8 @@ DEBUG = False
 default_manifest = ".repo/manifest.xml"
 
 custom_local_manifest = ".repo/local_manifests/roomservice.xml"
-custom_default_revision = "lp5.0"
+custom_default_revision = "lp5.1"
+custom.dependencies="orion.dependencies"
 org_manifest = "TeamOrion"  # leave empty if org is provided in manifest
 org_display = "TeamOrion"  # needed for displaying
 
