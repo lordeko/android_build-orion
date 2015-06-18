1846ad4c2fdcd9150c0281a07617bf3ba8bbbfa8
SohamJ <soham4jam@gmail.com>
2015-06-18 19:07:39 -0400

    Add CMFileManager
    Change-Id: I717c04f9b4493d1d30e237e42dac3142ab4a5c0a

--- target/product/core.mk
+++ target/product/core.mk
@@ -52,6 +52,7 @@ PRODUCT_PACKAGES += \
     Telecom \
     TeleService \
     VpnDialogs \
-    MmsService
+    MmsService \
+    CMFileManager
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_base.mk)
